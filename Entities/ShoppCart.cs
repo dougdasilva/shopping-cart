3d993505f47779f1179168a2681e4140f9ef97c8
dougdasilva <douglas_wfs@hotmail.com>
2022-05-23 21:31:31 -0300

    ajustes no método total

--- Entities/ShoppCart.cs
+++ Entities/ShoppCart.cs
@@ -9,16 +9,21 @@ namespace Shopp.Entities
     internal class ShoppCart
     {
         public int Quantity { get; set; }
-        public List<Product> Products { get; set; } = new List<Product>();
+        public List<OrderItem> Products { get; set; } = new List<OrderItem>();
 
         public ShoppCart()
         {
 
         }
 
-        public void Total()
+        public double Total()
         {
-
+            double sum = 0.0;
+            foreach (OrderItem order in Products)
+            {
+                sum += order.SubTotal();
+            }
+            return sum;
         }
 
     }
